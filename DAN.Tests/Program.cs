0627ffabb352412290171f7a04b980e3224bb910
cunwei.fan <cunwei.fan@31huiyi.com>
2018-09-27 10:32:55 +0800

    Test

--- DAN.Tests/Program.cs
+++ DAN.Tests/Program.cs
@@ -15,15 +15,16 @@ namespace DAN.Tests
         {
             //这里就不引入依赖注入了。
             Repository<User> repository = new Repository<User>();
-            for (var i = 0; i <= 100000; i++) {
+            for (var i = 0; i <= 100000; i++)
+            {
 
                 repository.Update(new User()
                 {
                     CompanyId = 13232,
                     Mobile = "11111" + i
                 });
-                }
-           Console.WriteLine($"Count:{DANQueue<User>.Count()}");
+            }
+            Console.WriteLine($"Count:{DANQueue<User>.Count()}");
             Console.ReadLine();
         }
     }
--- DAN/Timer/TimerJob.cs
+++ DAN/Timer/TimerJob.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using DAN.CollectionQueue;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 
 namespace DAN.Timer
 {
@@ -9,5 +11,18 @@ namespace DAN.Timer
     /// </summary>
    public class TimerJob:IJob
     {
+        public System.Timers.Timer aTimer;
+        public TimerJob() {
+            aTimer = new System.Timers.Timer(DANConfig.Interval);
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            //DANQueue<IEntity>.TryTakeAll();
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
     }
 }
